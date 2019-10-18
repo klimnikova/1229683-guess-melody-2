e875a013eab21a8b96d2ce66d397387b7759e09c
Климникова Мария Сергеевна <klimnikova_m@dalzaso.local>
2019-10-18 11:49:53 +1000

    Учебный проект: начинаем программировать

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+
+const init = () => {
+  ReactDOM.render(
+      <App />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
